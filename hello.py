c70bf10a05e0b15566b4d8b61b89b3cb42255e2a
AWADYY <patryk.dudzinski@outlook.com>
2022-04-18 16:02:59 +0200

    CS50P - python course

---
+++ hello.py
@@ -0,0 +1,6 @@
+# Ask user for their name
+# Remove whitespace from str and capitalize user's name
+name = input("what's your name? ").strip().title()
+
+# Say Hello to user
+print(f"hello, {name}")
